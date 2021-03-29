ff12afbafadb513f4424da92fbf9aab2f3b9ddb8
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2021-03-29 18:10:32 +0100

    Created NegaMax, alpha-beta, and ran some tests

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Try profiling the code at some point
-# One Player I could make is one that outputs results of a saved game
 import copy
 import string
 import numpy as np
+import time
 
 LENGTH = 19
 WIDTH = 15
@@ -19,7 +19,8 @@ DIRECTIONS = {
     "SE": np.array((1, 1)),
     "NE": np.array((-1, 1)),
 }
-# I have had lots of fun dealing with formatting problems caused by circle sizes
+# I have had lots of fun dealing with
+# formatting problems caused by circle sizes
 SMALL_WHITE_CIRCLE = "⚬"  # Looks good on WordPress
 SMALL_BLACK_CIRCLE = "•"
 WHITE_CIRCLE = "○"  # Looks good on GitHub
@@ -54,7 +55,7 @@ class Board:
         self.ball_at = ball_at
         self.side_to_move = side_to_move
         self.moves_made = moves_made
-        if array != None:
+        if array is not None:
             self.array = copy.deepcopy(array)
         else:
             self.array = [
@@ -188,19 +189,21 @@ class Board:
                         moves[name] = new
         return moves
 
-    def get_ball_moves(self):
-        moves = {}
-        current_name = ""
-        return self.recursive_get_ball_moves(
-            moves, current_name
-        )
-
-    # I have to wrap the recursive method in this one,
-    # else I get a curious problem with the namespace.
-
-    def recursive_get_ball_moves(self, moves, current_name):
-        """Recursive method. Note that 'self' changes"""
+    def get_ball_moves(self, moves=None, current_name=""):
+        """Recursive method. Note that 'self' changes. Also, "Default
+        parameter values are evaluated from left to right when the
+        function definition is executed", so it's unwise to make
+        ~moves~ mutable, because then it gets changed, and Python will
+        keep using it as the default.
+        See
+        https://docs.python.org/3/reference/compound_stmts.html#function-definitions
+        """
         #        print(moves)
+        if moves is None:
+            moves = {}
+        if self.ball_at[1] in (-1, LENGTH):
+            # We jumped off the endzones and can't make any more jumps
+            return moves
         for direction in DIRECTIONS.keys():
             vec = DIRECTIONS[direction]
             new = self.copy()
@@ -218,7 +221,8 @@ class Board:
             if not new.is_on_board(end_point) and (
                 end_point - vec
             )[1] not in (0, LENGTH - 1):
-                # "It is also legal for the ball to leave the board, but only by jumping over a man on the goal line"
+                # "It is also legal for the ball to leave the board,
+                # but only by jumping over a man on the goal line"
                 continue  # We jumped off the board illegally
             for i in range(jump_length):
                 temp = (
@@ -238,9 +242,7 @@ class Board:
             moves[new_name] = new.copy().increment()
             #            print(moves)
             # Now look for jump moves that started with this jump
-            moves = new.recursive_get_ball_moves(
-                moves, new_name
-            )
+            moves = new.get_ball_moves(moves, new_name)
         #            print(moves)
         return moves
 
@@ -251,6 +253,11 @@ class Board:
 
     def get_all_moves(self):
         moves = self.get_man_moves()
+        moves.update(self.get_ball_moves())
+        return moves
+
+    def get_all_nearby_moves(self):
+        moves = self.get_nearby_man_moves()
         moves.update(self.get_ball_moves())
         return moves
 
@@ -295,13 +302,21 @@ def run_game(players=[HumanPlayer(), HumanPlayer()]):
     num_moves_made = 0
     moves_made = []
     current_board = Board()
+    current_board.pretty_print_details()
+    start_time = time.time()
     while True:
         player = players[num_moves_made % 2]
         move = player.make_move(current_board.copy())
         moves_made.append(move)
         print(moves_made)
+        print(
+            "Duration of game so far is",
+            time.time() - start_time,
+            "seconds",
+        )
         #        current_board.pretty_print_details()
         current_board = current_board.get_all_moves()[move]
+        current_board.pretty_print_details()
         # Previous line makes the move, and returns an error if invalid
         if current_board.ball_at[1] <= 0:
             print("Right has won")
@@ -310,3 +325,187 @@ def run_game(players=[HumanPlayer(), HumanPlayer()]):
             print("Left has won")
             return
         num_moves_made += 1
+
+
+class LocationEvaluator:
+    """Returns the normalized position of the ball. The score is close to 1 if the
+    ball is near the goal of the player to move. It's 0 in the converse case
+
+    """
+
+    def score(self, board):
+        location = board.ball_at[1]
+        if location <= 0:
+            location = 0
+        if location >= LENGTH - 1:
+            location = LENGTH - 1
+        value = location / (LENGTH - 1)
+        # This is near 1 if we're to the east,
+        # near the goal Right is protecting
+        if board.side_to_move == "Left":
+            return value
+        else:
+            return 1 - value
+
+
+class NegamaxPlayer:
+    def __init__(
+        self, depth=1, static_evaluator=LocationEvaluator()
+    ):
+        self.depth = depth
+        self.static_evaluator = static_evaluator
+
+    def score(self, board, depth):
+        """Returns the score of the player to move"""
+        if board.ball_at[1] in (
+            0,
+            -1,
+            LENGTH - 1,
+            LENGTH,
+        ):
+            # If we've won/lost, we need to stop thinking and
+            # accurately report it
+            return LocationEvaluator().score(board)
+        if depth == 0:
+            # Don't keep thinking if we've run out of depth
+            self.calls += 1
+            return self.static_evaluator.score(board)
+        max_score = 0
+        possible_moves = board.get_all_nearby_moves()
+        for board in possible_moves.values():
+            temp_score = self.score(board, depth - 1)
+            corrected_score = 1 - temp_score
+            # We are the player to move, so we maximize
+            # the score of our move
+            if corrected_score >= max_score:
+                max_score = corrected_score
+        return max_score
+
+    def make_move(self, board):
+        self.calls = 0
+        print(
+            "Applying static evaluator to current position:"
+        )
+        print(self.static_evaluator.score(board))
+        max_move = None
+        max_score = 0
+        print("Initial score is 0")
+        possible_moves = board.get_all_nearby_moves()
+        for move in possible_moves.keys():
+            temp_score = self.score(
+                possible_moves[move], self.depth - 1
+            )
+            corrected_score = 1 - temp_score
+            # We are the player to move, so we maximize
+            # the score of our move
+            if corrected_score >= max_score:
+                max_score = corrected_score
+                max_move = move
+                print(
+                    "New best move is",
+                    max_move,
+                    "which has score of",
+                    max_score,
+                )
+        print(self.calls)
+        return max_move
+
+
+class NegamaxABPlayer:
+    def __init__(
+        self, depth=1, static_evaluator=LocationEvaluator()
+    ):
+        self.depth = depth
+        self.static_evaluator = static_evaluator
+
+    def score(self, board, depth, alpha, beta):
+        """Returns the score of the player to move"""
+        if board.ball_at[1] in (
+            0,
+            -1,
+            LENGTH - 1,
+            LENGTH,
+        ):
+            # If we've won/lost, we need to stop thinking and
+            # accurately report it
+            return LocationEvaluator().score(board)
+        if depth == 0:
+            # Don't keep thinking if we've run out of depth
+            self.calls += 1
+            return self.static_evaluator.score(board)
+        value = 0
+        possible_moves = board.get_all_nearby_moves()
+        # Don't reorder the moves because we're not at the top level
+        for board in possible_moves.values():
+            corrected_score = 1 - self.score(
+                board, depth - 1, 1 - beta, 1 - alpha
+            )
+            # We are the player to move, so we maximize
+            # the score of our move
+            value = max(corrected_score, value)
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                # We can guarantee a score better than beta,
+                # which is the score our opponent can guarantee
+                # by not choosing this node. So they won't choose this node.
+                if depth > 1:
+                    print(
+                        "Pruned, depth = ",
+                        depth,
+                        "alpha = ",
+                        alpha,
+                        "beta =",
+                        beta,
+                    )
+                break
+        return value
+
+    def make_move(self, board):
+        self.calls = 0
+        print(
+            "Applying static evaluator to current position:"
+        )
+        print(self.static_evaluator.score(board))
+        max_move = None
+        max_score = 0
+        print("Initial score is 0")
+        possible_moves = board.get_all_nearby_moves()
+        move_list = list(possible_moves.keys())
+        if self.depth > 1 and False:
+            # Sort the moves using a shallower search
+            move_list.sort(
+                key=lambda m: 1
+                - self.score(
+                    possible_moves[m],
+                    self.depth - 2,
+                    alpha=0,
+                    beta=1,
+                ),
+                reverse=True,
+            )
+        print(move_list)
+        for move in move_list:
+            temp_score = self.score(
+                possible_moves[move],
+                self.depth - 1,
+                alpha=0,
+                beta=1,
+            )
+            corrected_score = 1 - temp_score
+            # We are the player to move, so we maximize
+            # the score of our move
+            if corrected_score >= max_score:
+                max_score = corrected_score
+                max_move = move
+                print(
+                    "New best move is",
+                    max_move,
+                    "which has score of",
+                    max_score,
+                )
+            if max_score == 1:
+                # This is the alpha-beta break, except at the
+                # top level it just means we've won
+                break
+        print(self.calls)
+        return max_move
