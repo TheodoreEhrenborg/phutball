d677947e9efd6efa2b2ed3c52c5bdf04e58f0fcf
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2021-09-22 15:49:26 +0100

    More bug finding

---
+++ 2021-09-22-diagnostic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import main
+import random
+import time
+import pickle
+
+player = main.MCTSPlayer3(
+    num_evals=100000,
+    curiosity=0.001,
+    quiet=True,
+    name="saved_model/2021-08-23-v3-model",
+    pickle_in="2021-09-18-mcts-v3-boards13-random.pickle",
+)
+
+
+b = main.Board()
+
+count = 0
+
+while True:
+    count += 1
+    b.randomize(random.randint(0, 30))
+    b.pretty_print_details()
+    with open(
+        "2021-09-22-malfunctioning-board.pickle", "wb"
+    ) as f:
+        pickle.dump(b, f)
+    player.make_move(b.copy())
+    if count % 1 == 0:
+        print(count, "boards at ", time.strftime("%c"))
